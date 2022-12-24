34755b390ac0213359fcddac2f0d87c15489ebb2
Vadim <112751229+JustKingYT1@users.noreply.github.com>
2022-12-24 11:57:48 +0300

    24.12.2022

--- src/server/resolvers/expeditions.py
+++ src/server/resolvers/expeditions.py
@@ -2,7 +2,29 @@ from server.sql_base.db_manager import base_worker
 from server.sql_base.models import Expedition
 
 
-def create_expeditions(expedition: Expedition):
+def create_expedition(expedition: Expedition):
     return base_worker.execute(query="INSERT INTO expeditions(firm_id, date, location) VALUES (?, ?, ?) RETURNING id",
                                args=(expedition.firm_id, expedition.date, expedition.location))
+
+
+def get_expedition(expedition_id: int ):
+    return base_worker.execute_query(query="SELECT id, date, location FROM expeditions WHERE id = ?",
+                                     args=(expedition_id,))
+
+
+def get_all_expeditions():
+    return base_worker.execute_query(query="SELECT id, date, location FROM expeditions",
+                                     many=True)
+
+
+def update_expedition(expedition_id: int, new_data: Expedition):
+    return base_worker.execute_query(query="UPDATE expeditions SET (date, location) = (?, ?) WHERE id=?",
+                                     args=(new_data.date, new_data.location, expedition_id))
+
+
+def delete_expedition(expedition_id: int):
+    return base_worker.execute_query(query="DELETE FROM expeditions WHERE id=?",
+                                     args=(expedition_id,))
+
+
 
